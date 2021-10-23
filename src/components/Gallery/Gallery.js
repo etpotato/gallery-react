6a2bee05ced11b17b45d6f1cfe645eea57f28fdf
etpotato <mareev.pv@gmail.com>
2021-10-23 22:18:48 +0300

    Fix gallery text

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,12 +2,12 @@ import Navbar from '../Navbar/Navbar';
 import GalleryItem from '../GalleryItem/GalleryItem';
 import './gallery.scss';
 
-const Gallery = ({ photos, addToCart, removeFromCart, cart, openModal, tags, setTag }) => {
+const Gallery = ({ photos, addToCart, removeFromCart, cart, openModal, tags, searchValue, setSearchValue }) => {
   return (
     <div className='container container--main-wrap'>
       <main className='gallery pt-4 pb-5'>
-        <h1 className='lead text-center mb-3'>Browse photos and pick your favorite!</h1>
-        <Navbar tags={tags} setTag={setTag}/>
+        <h1 className='lead text-center mb-3'>Browse photos and get your favorite!</h1>
+        <Navbar tags={tags} searchValue={searchValue} setSearchValue={setSearchValue}/>
         <ul className='gallery__list row mb-2'>
           {photos.map(photo => {
             const isInCart = cart.some(cartPhoto => cartPhoto.id === photo.id);
