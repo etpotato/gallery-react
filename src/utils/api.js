cc056ab5ab6f1440b8b0d8010ee176304189aa65
etpotato <mareev.pv@gmail.com>
2021-10-24 21:39:00 +0300

    Fix api error handling

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,26 @@
 const API_KEY = '563492ad6f91700001000001647246300ed247b7b6748ac61d528807';
-const DEFAULT_URL = 'https://api.pexels.com/v1/curated?page=1&per_page=20';
 
-const getSearchUrl = (keyword, page = 1) => {
+const getDefaultUrl = (page) => {
+  return `https://api.pexels.com/v1/curated?page=${page}&per_page=20`;
+};
+
+const getSearchUrl = (keyword, page) => {
   return `https://api.pexels.com/v1/search?query=${keyword}&page=${page}&per_page=20`;
 };
 
-export default async function fetchPhotos (keyword, page = 1) {
-  let url = DEFAULT_URL;
-  if (keyword) url = getSearchUrl(keyword, page);
+export default async function fetchPhotos (keyword, page = 1, onSuccess) {
+  const url = keyword ? getSearchUrl(keyword, page) : getDefaultUrl(page);
   try {
+    console.log(`fetch: keyword-${keyword}, page-${page}`);
     const response = await fetch(url, {
       headers: { 'Authorization': API_KEY },
     });
-    if (!response.ok) throw new Error('fetch error');
-    return response.json();
+    if (response.ok) {
+      const data = await response.json();
+      return onSuccess(data);
+    } 
+    throw new Error('Fetch error ' + response.status);
   } catch (error) {
-    console.log(error);
+    console.error(error);
   }
 };
