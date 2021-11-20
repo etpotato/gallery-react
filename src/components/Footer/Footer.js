8d26b548c9363ca56e6e34010f3d4b061a45de67
Pavel Mareev <mareev.pb@gmail.com>
2021-11-20 11:16:10 +0300

    Tweak dynamic footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import useScrollDirection from '../../hooks/useScrollDirection';
 import APP from '../../config';
 import ScrollUpButton from '../ScrollUpButton/ScrollUpButton';
@@ -9,10 +9,10 @@ const ACTIVE_CLASS = 'show';
 
 export default function Footer () {
   const [modif, setModif] = useState('');
-  // const height = window.innerHeight ;
+  const heightRef = useRef(window.innerHeight);
 
-  const onScrollDown = (scrollY) => {
-    if (scrollY > APP.SCROLL_Y_BOUND) {
+  const onScrollDown = scrollY => {
+    if (scrollY > heightRef.current * 2 + APP.SCROLL_Y_BOUND) {
       setModif(ACTIVE_CLASS);
     }
   };
@@ -25,12 +25,22 @@ export default function Footer () {
 
   return (
     <footer className={`footer pt-2 pb-2 font-weight-light ${modif}`}>
-      <div className='container'>
+      <div className="container">
         <ScrollUpButton />
-        <div className="row">
-          <a className='footer__link col-sm-auto p-1' href="https://www.pexels.com" target='_blank' rel='noreferrer'>Photos provided by Pexels</a>
-          <p className='footer__copy col p-1'>copyleft 2021</p>
-          <a className="footer__link col-sm-auto p-1" href="mailto:mareev.pv@gmail.com">mareev.pv@gmail.com</a>
+        <div className="row justify-content-between">
+          <a className="footer__link col-sm-auto p-1"
+            href="https://www.pexels.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Photos provided by Pexels
+          </a>
+          <p className="footer__copy d-sm-block col p-1">copyleft 2021</p>
+          <a className="footer__link col-sm-auto p-1"
+            href="mailto:mareev.pv@gmail.com"
+          >
+            mareev.pv@gmail.com
+          </a>
         </div>
       </div>
     </footer>
