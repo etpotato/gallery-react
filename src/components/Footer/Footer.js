ef3cea112a1cee48ea60f06bb5235bc6341821c7
Pavel Mareev <mareev.pb@gmail.com>
2021-11-23 21:15:06 +0300

    Fix container width

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,15 +8,15 @@ export default function Footer () {
       <div className="container">
         <ScrollUpButton />
         <div className="row justify-content-between">
-          <a className="footer__link col-sm-auto p-1"
+          <a className="footer__link col-sm-auto"
             href="https://www.pexels.com"
             target="_blank"
             rel="noreferrer"
           >
             Photos provided by Pexels
           </a>
-          <p className="footer__copy d-sm-block col p-1">copyleft 2021</p>
-          <a className="footer__link col-sm-auto p-1"
+          <p className="footer__copy d-sm-block col">copyleft 2021</p>
+          <a className="footer__link col-sm-auto"
             href="mailto:mareev.pv@gmail.com"
           >
             mareev.pv@gmail.com
