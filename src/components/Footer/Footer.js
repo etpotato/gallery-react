aaea0c391e7b0530f3de38ce0213396938b318ff
Pavel Mareev <mareev.pb@gmail.com>
2021-11-23 20:52:18 +0300

    Remove conditional footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,30 +1,10 @@
-import { useState, useRef } from 'react';
-import useScrollDirection from '../../hooks/useScrollDirection';
-import APP from '../../config';
 import ScrollUpButton from '../ScrollUpButton/ScrollUpButton';
 
 import './footer.scss';
 
-const ACTIVE_CLASS = 'show';
-
 export default function Footer () {
-  const [modif, setModif] = useState('');
-  const heightRef = useRef(window.innerHeight);
-
-  const onScrollDown = scrollY => {
-    if (scrollY > heightRef.current * 2 + APP.SCROLL_Y_BOUND) {
-      setModif(ACTIVE_CLASS);
-    }
-  };
-
-  const onScrollUp = () => {
-    setModif('');
-  };
-
-  useScrollDirection( onScrollDown, onScrollUp );
-
   return (
-    <footer className={`footer pt-2 pb-2 font-weight-light ${modif}`}>
+    <footer className='footer pt-2 pb-2 font-weight-light'>
       <div className="container">
         <ScrollUpButton />
         <div className="row justify-content-between">
